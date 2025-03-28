d1870b022236346f2e1ae8f8b15bb8f16fa10bf9
Lucas Meireles <lucas.santana962@gmail.com>
2025-03-28 20:39:22 -0300

    Logotipo

--- js/main.js
+++ js/main.js
@@ -1,37 +1,4 @@
-/* =====================================
-Template Name: 	Mediplus.
-Author Name: Naimur Rahman
-Website: http://wpthemesgrid.com/
-Description: Mediplus - Doctor HTML Template.
-Version:	1.1
-========================================*/   
-/*=======================================
-[Start Activation Code]
-=========================================
-* Sticky Header JS
-* Search JS
-* Mobile Menu JS
-* Hero Slider JS
-* Testimonial Slider JS
-* Portfolio Slider JS
-* Clients Slider JS
-* Single Portfolio Slider JS
-* Accordion JS
-* Nice Select JS
-* Date Picker JS
-* Counter Up JS
-* Checkbox JS
-* Right Bar JS
-* Video Popup JS
-* Wow JS
-* Scroll Up JS
-* Animate Scroll JS
-* Stellar JS
-* Google Maps JS
-* Preloader JS
-=========================================
-[End Activation Code]
-=========================================*/ 
+
 (function($) {
     "use strict";
      $(document).on('ready', function() {
@@ -329,3 +296,19 @@ Version:	1.1
 	
 	
 })(jQuery);
+
+  window.onload = function() {
+    // Verifica se o link está correto ao clicar
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+      link.addEventListener('click', function(event) {
+        const href = this.getAttribute('href');
+        if (!document.querySelector(href)) {
+          event.preventDefault();
+          window.location.href = '404.html';
+        }
+      });
+    });
+  };
+
+  
